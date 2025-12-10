9326a9d8d6528c4c9141d468d8f7422f768197c4
gkalu <gracekalu660@gmail.com>
2025-12-10 12:12:13 +0100

    Quest 11

---
+++ Quest_11/listat.go
@@ -0,0 +1,17 @@
+package piscine_go
+
+func ListAt(l *NodeL, pos int) *NodeL {
+	if l == nil || pos < 0 {
+		return nil
+	}
+	current := l
+	index := 0
+	for current != nil {
+		if index == pos {
+			return current
+		}
+		current = current.Next
+		index++
+	}
+	return nil
+}
---
+++ Quest_11/listmerge.go
@@ -0,0 +1,15 @@
+package piscine_go
+
+func ListMerge(l1 *List, l2 *List) {
+	if l1 == nil || l2 == nil || l2.Head == nil {
+		return
+	}
+
+	if l1.Head == nil {
+		l1.Head = l2.Head
+		l1.Tail = l2.Tail
+	} else {
+		l1.Tail.Next = l2.Head
+		l1.Tail = l2.Tail
+	}
+}
